536f642f4224623cc28ffb097fa948f63b943c66
DylanFurner-71 <jdfurner@gmail.com>
2021-03-02 00:30:20 -0600

    basic, basic admin and student functionality. can't figure out how to jest test wiith this stack. very frustrated

---
+++ backend/db.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+var config = {
+  user: 'root', //this has to be changed before deployment but i don't know how
+  password: 'password',
+  server: 'localhost', 
+  database: 'db1',
+  port: "3306",
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+  /*
+  dbhost  : lyledb1.seas.smu.edu
+  database: hclsdata
+  username: hartadmin
+  password: hh77gg99
+  */
+};
+// mysql connection
+var pool = mysql.createPool({
+//   host: process.env.MYSQL_CLOUD_HOST,
+//   user: process.env.MYSQL_CLOUD_USER,
+//   password: process.env.MYSQL_CLOUD_PASS,
+//   port: process.env.MYSQL_PORT,
+//   database: process.env.MYSQL_DB
+host: config.server,
+user: config.user,
+password: config.password,
+port: config.port,
+database: config.database
+});
+
+module.exports = pool;
---
+++ backend/index.js
@@ -0,0 +1,5 @@
+// const app = require("./server.js");
+// app.listen(8000, () => {
+//   console.log("Server has started!");
+// });
+// export default app;
