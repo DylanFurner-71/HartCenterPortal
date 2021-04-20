df239eba7829f8a5380b2ce409ce3d602c97c364
DylanFurner-71 <jdfurner@gmail.com>
2021-04-20 15:18:14 -0500

    switching to omar branch

--- frontend/hartleadershipfe/src/routes.js
+++ frontend/hartleadershipfe/src/routes.js
@@ -5,10 +5,17 @@ import CompetencyLibrary from "./components/student/CompetencyLibrary"
 import StudentHome from "./components/student/studentLanding";
 import ContactUs from './components/student/ContactUs.js';
 import IntentionalLearning from "./components/student/IntentionalLearning";
+import AboutUs from "./components/info/AboutUs";
+import HartLeadershipFramework from "./components/info/HartLeadershipFramework";
+import HartLeadershipInfo from "./components/info/HartLeadershipInfo";
 export const ROUTES = [
     { path: '/', component: Landing },
     { path: '/student/home', component: StudentHome },
     { path: '/competency/library', component: CompetencyLibrary},
     { path: '/contact/', component: ContactUs},
-    { path: '/competency/library/Intentional%20Learning', component: IntentionalLearning}
+    { path: '/competency/library/Intentional%20Learning', component: IntentionalLearning},
+    { path: '/about/', component: AboutUs},
+    { path: '/about/fmwk', component: HartLeadershipFramework},
+    { path: '/about/surveyinfo', component: HartLeadershipInfo},
+
 ]
